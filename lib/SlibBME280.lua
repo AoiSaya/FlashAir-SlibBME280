7813ad18e45b66a3223eada1520090250e3ecb6d
AoiSaya <cool@blue.kirara.st>
2018-09-13 00:40:18 +0900

    debug humidity & setup etc.

--- lib/SlibBME280.lua
+++ lib/SlibBME280.lua
@@ -1,6 +1,8 @@
 -----------------------------------------------
--- BME280 control libraly for FlashAir W-04
--- 2018/09/11 rev.0.04 I2C control including
+-- Libraly of BME280 control for W4.00.03
+-- Copyright (c) 2018, Saya
+-- All rights reserved.
+-- 2018/09/13 rev.0.05 humidity & setup debug etc.
 -----------------------------------------------
 local BME280 = {
 	SADR = 0x76;
@@ -14,6 +16,7 @@ function BME280:write( ... )
 	local res = fa.i2c{ mode="start", address=self.SADR, direction="write" }
 	local res = fa.i2c{ mode="write", data=string.char(...) }
 	local res = fa.i2c{ mode="stop" }
+
 	return res
 end
 
@@ -28,6 +31,8 @@ function BME280:wreadt( adr, len )
 	return res, {str:byte(1, #str)}
 end
 
+-- Return temperature in DegC. Output value of g51.23h equals 51.23 DegC.
+-- t_fine carries fine temperature as self.TRIM
 function BME280:calibration_T( adc_T )
 	local var1 = (adc_T/16384.0 - self.TRIM.T1/1024.0)
 	local var2 =  var1		* self.TRIM.T2
@@ -41,6 +46,7 @@ function BME280:calibration_T( adc_T )
 	return temp
 end
 
+// Return pressure in hPa. Output value of g963.86\2h equals 963.862 hPa
 function BME280:calibration_P( adc_P )
 	local var1 = (self.TRIM.t_fine / 2.0) - 64000.0
 	local var2 = var1^2 * self.TRIM.P6 / 32768.0
@@ -51,7 +57,7 @@ function BME280:calibration_P( adc_P )
 	local var1 = (1.0 + var1 / 32768.0) * self.TRIM.P1
 
 	if (var1 == 0) then
-		pres = 30000.0
+		pres = 300.0
 	else
 		pres = (1048576.0 - adc_P - (var2 / 4096.0)) * 6250.0 / var1
 		var1 = self.TRIM.P9 * pres^2 / 2147483648.0
@@ -66,9 +72,10 @@ function BME280:calibration_P( adc_P )
 	return pres
 end
 
+-- Return humidity in %rH. Output value of g46.332h represents 46.332 %rH
 function BME280:calibration_H( adc_H )
 	var1 = self.TRIM.t_fine - 76800.0
-	var2 = self.TRIM.H4 * 64.0 - self.TRIM.H5 / 16384.0
+	var2 = self.TRIM.H4 * 64.0 + self.TRIM.H5 / 16384.0
 	var3 = adc_H - var2
 	var4 = self.TRIM.H2 / 65536.0
 	var5 = 1.0 + (self.TRIM.H3 / 67108864.0) * var1
@@ -81,18 +88,56 @@ function BME280:calibration_H( adc_H )
 	return humi
 end
 
+function BME280:readTrim()
+	local res, da = self:wreadt( 0x88, 24 )
+	local res, db = self:wreadt( 0xA1, 1  )
+	local res, dc = self:wreadt( 0xE1, 7  )
+	local function u2s(x,n)
+		if (x>=2^(n-1)) then x = x-2^n end
+		return x
+	end
+
+	if res!="OK" then
+		return res
+	end
+	self.TRIM = {
+		T1 =	 (da[2]  * 2^8) + da[1] 	 ;
+		T2 = u2s((da[4]  * 2^8) + da[3],  16);
+		T3 = u2s((da[6]  * 2^8) + da[5],  16);
+		P1 =	 (da[8]  * 2^8) + da[7] 	 ;
+		P2 = u2s((da[10] * 2^8) + da[9],  16);
+		P3 = u2s((da[12] * 2^8) + da[11], 16);
+		P4 = u2s((da[14] * 2^8) + da[13], 16);
+		P5 = u2s((da[16] * 2^8) + da[15], 16);
+		P6 = u2s((da[18] * 2^8) + da[17], 16);
+		P7 = u2s((da[20] * 2^8) + da[19], 16);
+		P8 = u2s((da[22] * 2^8) + da[21], 16);
+		P9 = u2s((da[24] * 2^8) + da[23], 16);
+		H1 =	  db[1];
+		H2 = u2s((dc[2]  * 2^8) + dc[1],  16);
+		H3 =	  dc[3];
+		H4 = u2s((dc[4]  * 2^4) + (dc[5] % 2^4), 12);
+		H5 = u2s((dc[6]  * 2^4) + ((dc[5] - dc[5] % 2^4) / 2^4), 12);
+		H6 = u2s( dc[7], 8);
+		t_fine = 0
+	}
+
+	return res
+end
+
 --[For user functions]--
 
 --BME280 setup
-function BME280:setup(sadr) -- I2C only
+--
+-- mode:	Mode
+-- t_sb:	Tstandby
+-- filter:	Filter coeficient
+-- osrs_t:	Temperature oversampling
+-- osrs_p:	Pressure oversampling
+-- osrs_h:	Humidity oversampling
+--
+function BME280:setup(sadr, mode, t_sb, filter, osrs_t, osrs_p, osrs_h)
 	self.SADR = sadr
-
-	local osrs_t = 4	-- Temperature oversampling x 8
-	local osrs_p = 4	-- Pressure oversampling x 8
-	local osrs_h = 4	-- Humidity oversampling x 8
-	local mode	 = 3	-- Normal mode
-	local t_sb	 = 0	-- Tstandby 125ms
-	local filter = 0	-- Filter coef=4
 	local spi3w_en = 0	-- 3-wire SPI Disable
 
 	local ctrl_meas = (osrs_t * 2^5) + (osrs_p * 2^2) + mode
@@ -108,42 +153,22 @@ function BME280:setup(sadr) -- I2C only
 	return res
 end
 
-function BME280:readTrim()
-	local res, da = self:wreadt( 0x88, 24 )
-	local res, db = self:wreadt( 0xA1, 1  )
-	local res, dc = self:wreadt( 0xE1, 7  )
-	local function u2s(x,n)
-		if (x>=2^(n-1)) then x = x-2^n end
-		return x
+-- pres_sea: sea-level pressure [hPa]
+--
+-- Return values
+-- res: Same as lua.i2c return value.
+-- temp: temperature [DegC]
+-- humi: humidity [%rH]
+-- pres: pressure [hPa]
+-- alti: altitude [m]
+--
+function BME280:readData( pres_sea )
+	local res, d = self:wreadt( 0xF7, 8 )
+
+	if res!="OK" then
+		return res
 	end
 
-	self.TRIM = {
-		T1 =	 (da[2]  * 2^8) + da[1] 	 ;
-		T2 = u2s((da[4]  * 2^8) + da[3],  16);
-		T3 = u2s((da[6]  * 2^8) + da[5],  16);
-		P1 =	 (da[8]  * 2^8) + da[7] 	 ;
-		P2 = u2s((da[10] * 2^8) + da[9],  16);
-		P3 = u2s((da[12] * 2^8) + da[11], 16);
-		P4 = u2s((da[14] * 2^8) + da[13], 16);
-		P5 = u2s((da[16] * 2^8) + da[15], 16);
-		P6 = u2s((da[18] * 2^8) + da[17], 16);
-		P7 = u2s((da[20] * 2^8) + da[19], 16);
-		P8 = u2s((da[22] * 2^8) + da[21], 16);
-		P9 = u2s((da[24] * 2^8) + da[23], 16);
-		H1 =	  db[1];
-		H2 = u2s((dc[2]  * 2^8) + dc[1], 16);
-		H3 =	  dc[3];
-		H4 = u2s((dc[4]  * 2^4) + (dc[5] % 2^4), 16);
-		H5 = u2s((dc[6]  * 2^4) + ((dc[5] - dc[5] % 2^4) / 2^4), 16);
-		H6 = u2s( dc[7], 8);
-		t_fine = 0
-	}
-
-	return res
-end
-
-function BME280:readData( pres_sea )
-	local res, d   = self:wreadt( 0xF7, 8 )
 	local pres_raw = (d[1] * 2^12) + (d[2] * 2^4) + (d[3] / 2^4)
 	local temp_raw = (d[4] * 2^12) + (d[5] * 2^4) + (d[6] / 2^4)
 	local humi_raw = (d[7] * 2^8 ) + d[8]
